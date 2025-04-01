0490f803e1c1fa9edec316294d2c9753b3a45ee7
Trezapois <gabriel.decuyper@epita.fr>
2025-04-01 20:10:46 +0300

    Command line game possible maybe problem on heuristic for minimizer

--- Final.py
+++ Final.py
@@ -9,6 +9,7 @@ class State_s:
         self.level = level
         self.next = []
         self.hVal = None
+        self.chosen = None
         
 
     def rm_1(self):
@@ -85,6 +86,7 @@ class State_s:
             return self.split_2()
         if n == 5:
             return self.split_4()
+        return None
         
 
     
@@ -101,7 +103,9 @@ class State_s:
                         temp = next.Minimax(n_ply - 1)
                     else:
                         temp = next.heuristic()
-                    val = max(val,temp)
+                    if temp > val:
+                        val = temp
+                        self.chosen = i
                     #if val == 1:
                     #    return 1
             return val    
@@ -115,7 +119,9 @@ class State_s:
                         temp = next.Minimax(n_ply - 1)
                     else:
                         temp = next.heuristic()
-                    val = min(val,temp)
+                    if temp < val:
+                        val = temp
+                        self.chosen = i
                     #if val == -1:
                     #    return -1
             return val        
@@ -135,7 +141,10 @@ class State_s:
                     else:
                         temp = next.heuristic()
                         #print("1"*next.n_1 + "2"*next.n_2 +"4"*next.n_4 + " b: "+str(next.bank) + " p: " +str(self.p)+": " + str(temp) + "|"+ str(next.AlphaBeta()))
-                    val = max(val,temp)
+                    if temp > val:
+                        val = temp
+                        self.chosen = i
+                        
                     #if val == 1:
                     #    return 1
                     alpha = max(alpha,val)
@@ -154,12 +163,15 @@ class State_s:
                         
                         temp = next.heuristic()
                         #print("1"*next.n_1 + "2"*next.n_2 +"4"*next.n_4 + " b: "+str(next.bank) + " p: " +str(next.p)+": " + str(temp) + "|"+ str(next.AlphaBeta()))
-                    val = min(val,temp)
+                    if temp < val:
+                        val = temp
+                        self.chosen = i
                     #if val == -1:
                     #    return -1
                     beta = min(beta,val)
                     if alpha >= beta:
                         break
+            #print(self.chosen)
             return val    
     
     def AlphaBeta(self,alpha = -2,beta = 2):
@@ -178,14 +190,14 @@ class State_s:
                     #    temp = next.heuristic()
                     #if temp == -2:
                     #    print("----")                    
-                    val = max(val,temp)
+                    if temp > val:
+                        val = temp
+                        self.chosen = next
                     if val == 1:
                         return 1
                     alpha = max(alpha,val)
                     if alpha >= beta:
                         break
-            if val == -2:
-                print("1"*self.n_1 + "2"*self.n_2 +"4"*self.n_4 + " b: "+str(self.bank) + " p: " +str(self.p))
             return val    
         else:
             #Minimizer turn
@@ -193,12 +205,14 @@ class State_s:
             for i in range(6):
                 next = self.advance(i)
                 if next is not None:
-                    print("---")
+                    #print("---")
                     #if n_ply > 0:
                     temp = next.AlphaBeta()
                     #else:
                     #    temp = next.heuristic()
-                    val = min(val,temp)
+                    if temp < val:
+                        val = temp
+                        self.chosen = next
                     if val == -1:
                         return -1
                     beta = min(beta,val)
@@ -220,19 +234,70 @@ class State_s:
             if(self.n_4 == 1 and self.n_2 == 0 and (self.n_1 + self.n_3)%2 == 1):
                 x+=1
             return 1-((self.bank+self.n_2+self.n_4 + x)%2 + (self.p+self.n_1 + self.n_3)%2)
-            
+    
+    def print(self):
+        print("1"*self.n_1 + "2"*self.n_2 + "3"*self.n_3 +"4"*self.n_4 + " bank: " + str(self.bank) + " points: " + str(self.p))
+    
+    
+def game(state, AI_first):
+    state.print()
+    if AI_first:
+        state.AlphaBetaNply()
+        print(state.chosen)
+        state = state.advance(0)
+        state.print()
+    while state.n_1 + state.n_2 + state.n_3 + state.n_4 > 0:
+        print(state.heuristic())
+        c = int(input("what action : "))
+        while c == None or c < 0 or c > 5 or state.advance(c) == None:
+            c = int(input("what action : "))
+        state = state.advance(c)
+        state.print()
+        if state.n_1 + state.n_2 + state.n_3 + state.n_4 == 0:
+            break
+        state.AlphaBetaNply()
+        state = state.advance(state.chosen)
+        state.print()
+    p1 = "You"
+    p2 = "The AI"
+    if AI_first:
+        (p1,p2) = (p2,p1)
+    if(state.bank%2 == 0):
+        if state.p % 2 == 0:
+            print(p1 + " won")
+        else:
+            print("It's a tie")
+    else:
+        if state.p % 2 == 0:
+            print("It's a tie")
+        else:
+            print(p2 + " won")
+        
 
-for i in range(2):
+"""for i in range(2):
     for j in range(3):
         for k in range(3):
             A = State_s(i,0,j,k,2,0,0)
             #A.generate_next_states()
             print("1"*i + "2"*j +"4"*k + " : " + str(A.AlphaBetaNply()) + " | " + str(A.heuristic()))
-            
+    """        
             
 
-"""A = State_s(2,0,0,0,1,2,0)
+A = State_s(1,1,2,2,0,0,0)
 print(A.AlphaBeta())
-print(A.AlphaBetaNply())
-print(A.Minimax())
-print(A.heuristic())"""
+#print(A.AlphaBetaNply())
+#print(A.Minimax())
+print(A.heuristic())
+game(A,True)
+
+"""while A.n_1 + A.n_2 + A.n_3 + A.n_4 > 0:
+    A = A.advance(A.chosen)
+    print("1"*A.n_1 + "2"*A.n_2 + "3"*A.n_3 +"4"*A.n_4 + " bank: " + str(A.bank) + " points: " + str(A.p))
+    c = int(input("what action"))
+    while c == None or c < 0 or c > 5 or A.advance(c) == None:
+        c = int(input("what action"))
+    A = A.advance(c)
+    if A.n_1 + A.n_2 + A.n_3 + A.n_4 == 0:
+        break
+    print("1"*A.n_1 + "2"*A.n_2 + "3"*A.n_3 +"4"*A.n_4 + " bank: " + str(A.bank) + " points: " + str(A.p))
+    A.AlphaBetaNply()"""
